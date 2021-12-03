6f64ef6c5e1c253edd85f90a2ae33d9351b3d160
Gyan Prakash <gyanp008@gmail.com>
2021-12-03 23:34:38 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { HttpClientModule } from '@angular/common/http';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: EmployeeListComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    HttpClientModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
